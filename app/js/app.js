42c4ebf9bcff29de719ba467bd5be6511785dd34
Todd Meinershagen <todd@meinershagen.net>
2014-10-26 22:20:05 -0500

    Fixing up the angular on the index.html so that first test passes.

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,9 @@
 (function () {
    'use strict';
 
-   
+    var app = angular.module('calculatorApp', []);
+
+    app.controller('MainCtrl', function($scope) {
+      $scope.name = 'World';
+    });
 }());
